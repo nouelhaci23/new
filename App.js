6aed48a9f5e0c9d96616e4109b33aa9db3b65b9e
nouelhaci23 <126028521+nouelhaci23@users.noreply.github.com>
2023-02-21 18:20:22 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import IPhone1313Pro81 from "./screens/IPhone1313Pro81";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+import {
+  GestureHandlerRootView,
+  RectButton,
+} from "react-native-gesture-handler";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+  const [fontsLoaded, error] = useFonts({
+    Poppins: require("./assets/fonts/Poppins.ttf"),
+    Poppins_regular: require("./assets/fonts/Poppins_regular.ttf"),
+    Poppins_semibold: require("./assets/fonts/Poppins_semibold.ttf"),
+    Poppins_bold: require("./assets/fonts/Poppins_bold.ttf"),
+    Poppins_extrabold: require("./assets/fonts/Poppins_extrabold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          {hideSplashScreen ? (
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen
+                name="IPhone1313Pro81"
+                component={IPhone1313Pro81}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          ) : null}
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </>
+  );
+};
+export default App;
---
+++ GlobalStyles.js
@@ -0,0 +1,54 @@
+/* fonts */
+export const FontFamily = {
+  poppinsSemibold: "Poppins_semibold",
+  poppinsRegular: "Poppins_regular",
+  poppinsExtrabold: "Poppins_extrabold",
+  poppinsBold: "Poppins_bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_base: 10,
+  size_lg: 14,
+  size_xl: 16,
+  size_2xl: 19,
+  size_3xl: 20,
+  size_4xl: 24,
+  size_5xl: 40,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  chocolate_100: "#f07e2b",
+  chocolate_200: "rgba(221, 121, 3, 0.51)",
+  chocolate_300: "rgba(221, 121, 3, 0.22)",
+  black: "#000",
+  darkslateblue: "#6d62b4",
+  navy: "#0b0889",
+  maroon: "rgba(111, 24, 24, 0)",
+  darkgray_100: "#9b9b9b",
+  gainsboro: "#e8e8e8",
+  gray_100: "#fffefe",
+  slategray: "#788096",
+};
+/* Paddings */
+export const Padding = {
+  p_3xs: 1,
+  p_2xs: 4,
+  p_xs: 10,
+  p_sm: 11,
+  p_md: 12,
+  p_lg: 16,
+  p_xl: 17,
+  p_2xl: 32,
+};
+/* Margins */
+export const Margin = {
+  m_sm: 16,
+  m_md: 20,
+  m_lg: 40,
+};
+/* border radiuses */
+export const Border = {
+  br_sm: 5,
+  br_md: 10,
+};
